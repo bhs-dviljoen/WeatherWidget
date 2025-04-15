40e7224373dd3f586d24fc446650787365e61b93
bhs-dviljoen <dviljoen@bhs.org.za>
2025-04-15 15:36:15 +0200

    Initiated files from p5.js
    First draft of my Weather Widget app

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(81, 154, 158);
+  
+  // Draw Labels
+  textSize(25);
+  fill(255);
+  text("DIGITAL", 60, 50);
+}
+
+function keyPressed() {
+  // Activates when key is pressed
+  if (keyCode === 32) {
+    console.log("spacebar");
+  }
+  if (keyCode === 37) {
+    console.log("left arrow");
+  }
+  if (keyCode === 39) {
+    console.log("right arrow");
+  }
+}
